5e66c7ff31a180b39abdc0e1f04e62f9a47855bb
SDAMcIntyre <sdamcintyre@gmail.com>
2014-11-12 13:40:20 +1100

    Changed how blocks are written to the protocol file. Block 1 is the lead time, different stimuli should have block numbers > 1.

--- optacon.py
+++ optacon.py
@@ -12,7 +12,7 @@ zeros = numpy.zeros
 ceil = math.ceil
 
 
-def write_optacon_protocol_file(fileName,stimList,stimRep=[1],dwellTime=[4.35],leadTime=2000,stimName=[],
+def write_optacon_protocol_file(fileName,stimList,stimRep=[1],blockList=[2],dwellTime=[4.35],leadTime=2000,stimName=[' '],
         readPins=['A1','B1','C1','D1','E1','F1']):
     """This function writes your stimulus list to a text file that can be loaded by the optacon controller software
     Block" and "Stimulus" are terms used by the optacon software. Stimuli are nested within Blocks.
@@ -28,52 +28,55 @@ def write_optacon_protocol_file(fileName,stimList,stimRep=[1],dwellTime=[4.35],l
         *these variables should have the same length (if left at default it will be made to match length of other variables)
         """
      
-    # add blank stim to start of stim list
-    stimListPlusLead = []
-    stimListPlusLead += [zeros([24,6],int)]
-    stimListPlusLead += stimList
-    
     # quality control
+    if 1 in blockList:
+        sys.exit('blockList must not contain block number "1". It is reserved for the lead time inserted at the start of the protocol.')
     if len(stimRep)==1:
         stimRep = stimRep*len(stimList)
-    # add rep for blank stim at start of stim list
-    stimRepPlusLead =[]
-    stimRepPlusLead += [int(ceil(leadTime/dwellTime[0]))]
-    stimRepPlusLead += stimRep
+    if len(blockList)==1:
+        blockList = blockList*len(stimList)
     if len(dwellTime)==1:
-        dwellTime = dwellTime*len(stimListPlusLead)
-    if len(stimName)==0:
-        stimName = [' ']*len(stimListPlusLead)
-    stimName[0] = 'lead time'
-    if (len(stimListPlusLead)==len(dwellTime)==len(stimRepPlusLead)==len(stimName)) == False: #currently if user provides their own list this won't work because of the blank stim added at the start
+        dwellTime = dwellTime*len(stimList)
+    if len(stimName)==1:
+        stimName = stimName*len(stimList)
+    if (not len(stimList)==len(stimRep)==len(blockList)==len(dwellTime)==len(stimName)):
         sys.exit('stimulus lists must have equal length')
+
+    # add blank stim to start of protocol
+    stimList.insert(0,zeros([24,6],int))
+    stimRep.insert(0,int(ceil(leadTime/dwellTime[0])))
+    blockList.insert(0,1)
+    dwellTime.insert(0,4.35)    
+    stimName.insert(0,'lead time')
     # ----------------
     
     # combine all lists into master list
     masterStimList=[]
-    for i in range(len(stimListPlusLead)):
-        masterStimList.append({'stim':stimListPlusLead[i], 'stimNo':i+1, 
-                                    'dwellTime':dwellTime[i],'stimRep':stimRepPlusLead[i], 'stimName':stimName[i]})
+    for i in range(len(stimList)):
+        masterStimList.append({'stim':stimList[i], 'blockNo':blockList[i], 'stimNo':i+1, 
+                                    'dwellTime':dwellTime[i],'stimRep':stimRep[i], 'stimName':stimName[i]})
     #-----------------
     
     # write header
     dataFile = open(fileName+'.txt', 'w')
     dataFile.write('This is an "Optacon Stimulator" Data File.\nFile Format\t1\n' 
-                                        'nBlocks\t1\n')
+                                        'nBlocks\t%i\n' %len(set(blockList)))
     dataFile.write('Spare Line\nSpare Line\nSpare Line\nSpare Line\nSpare Line\n')
     dataFile.write('Pin Sync\t'+"\t".join("%s" %pin for pin in readPins)+'\n\n')
     # ------------
-    
+        
     # write blocks
     dataFile.write('Block No\t nStimuli\t nPresentations\n')
-    dataFile.write('%i\t%i\t%i\n'%(1,len(stimListPlusLead),1))
+    for b in set(blockList):
+        dataFile.write('%i\t%i\t%i\n'%(b,blockList.count(b),1))
     dataFile.write('\n') ## blank line between block description and stimuli
     # ------
-    
-    # stimuli
-    for thisStim in masterStimList:
+            
+    # write stimuli
+    for n in range(len(masterStimList)):
+        thisStim = masterStimList[n]
         dataFile.write('Row No\tBlock No\tStim No\tnRows\tDwell ms\tType\tnPresentations\tName\n')
-        dataFile.write('\t%i\t%i\t%i\t%f\t%i\t%i\t%s\n' %(1,thisStim['stimNo'],
+        dataFile.write('\t%i\t%i\t%i\t%f\t%i\t%i\t%s\n' %(thisStim['blockNo'],thisStim['stimNo'],
                                     24,thisStim['dwellTime'],0,thisStim['stimRep'],thisStim['stimName']))
         for row in range(0,24):
             dataFile.write('%i' %((row+1))) ## write row number at start of row
