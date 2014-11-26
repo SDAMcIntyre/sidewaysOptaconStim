a10e22b29fdb77508be4b159b409934ec87c0e3d
SDAMcIntyre <sdamcintyre@gmail.com>
2014-11-27 10:42:57 +1100

    Updated script for making stim files for pilot 1. Removed randomisation, and repeats of 'sham' stimulus since these things will be handled in the adaptive staircase. This now produces an optacon protocol file with each unique stimulus as a block in the protocol. Staircase procedures can call stimuli by block.

--- makeStimFilesForPilot1.py
+++ makeStimFilesForPilot1.py
@@ -22,8 +22,10 @@ from optacon import *
 from optaconSideways import *
 from psychopy import data
 import numpy
+import os
 
 zeros = numpy.zeros
+sign = numpy.sign
 
 presentationTime = 3000
 pauseTime = 1000
@@ -40,13 +42,14 @@ stimCombinations = [{'adaptStandard':adaptStandard,
     'testComparison':testComparison,
     'standardLocation':standardLocation} for 
     standardLocation in ['left','right'] for 
-    adaptStandard in [-82,82,'sNone','sNone'] for 
-    adaptComparison in [-82,82,'cNone','cNone'] for 
+    adaptStandard in [-82,82,'sNone'] for 
+    adaptComparison in [-82,82,'cNone'] for 
     testStandard in [-82,82,'random'] 
     for testComparison in [-104,-82,-60,-39,-17,17,39,60,82,104,'random'] 
     if adaptStandard != adaptComparison and type(adaptStandard) == type(adaptComparison) == type(testStandard) == type(testComparison)]
 
-trials = data.TrialHandler(stimCombinations,nReps=nReps)
+trials = data.TrialHandler(stimCombinations,nReps=nReps,method='sequential')
+trials.data.addDataType('blockNo')
 
 stimList = []
 repList = []
@@ -57,25 +60,21 @@ blockNo = 1
 for thisTrial in trials:
     
     blockNo += 1 #starts at 2 because 1 is reserved for lead time
+    trials.data.add('blockNo', blockNo)
     
     #pause
     pauseStim = [zeros([24,6],int)]
     pauseRep = [time_to_frames(pauseTime)]
     pauseName = ['pause']
     
-    #response
-    respStim = [zeros([24,6],int)]
-    respRep = [time_to_frames(responseTime)]
-    respName = ['response']
-    
     if thisTrial['testStandard'] == 'random':
         testStim, testRep = single_presentation(presDur=presentationTime,randomPos=[True,True])
         testName = ['random']*len(testStim)
         
-        stimList += testStim + respStim   
-        repList += testRep + respRep
-        nameList += testName + respName
-        blockList += [blockNo] * (len(testStim) + len(respStim))
+        stimList += testStim
+        repList += testRep
+        nameList += testName
+        blockList += [blockNo] * (len(testStim))
         
     else:
         
@@ -100,12 +99,12 @@ for thisTrial in trials:
         testName = ['test_STDLOC'+str(thisTrial['standardLocation'])+'_STDISOI'+str(thisTrial['testStandard'])+'_CMPISOI'+str(thisTrial['testComparison'])]
         testName = testName*len(testStim)
         
-        stimList += adaptStim + pauseStim + testStim + respStim   
-        repList += adaptRep + pauseRep + testRep + respRep
-        nameList += adaptName + pauseName + testName + respName
-        blockList += [blockNo] * (len(adaptStim) + len(pauseStim) + len(testStim) + len(respStim))
+        stimList += adaptStim + pauseStim + testStim
+        repList += adaptRep + pauseRep + testRep
+        nameList += adaptName + pauseName + testName
+        blockList += [blockNo] * (len(adaptStim) + len(pauseStim) + len(testStim))
     
 #trials.printAsText(stimOut=['adaptStandard','adaptComparison','testStandard','testComparison','standardLocation'])
-trials.saveAsText(fileName=exptFolder+'/pilot1_stimLog_SM', stimOut=['adaptStandard','adaptComparison','testStandard','testComparison','standardLocation'])    
+trials.saveAsText(fileName=exptFolder+'/pilot1_stimLog_SM', stimOut=['adaptStandard','adaptComparison','testStandard','testComparison','standardLocation'], dataOut=['blockNo_raw'])
 
 write_protocol_file(fileName=exptFolder+'/pilot1_protocols_SM',stimList=stimList,stimRep=repList,blockList=blockList,stimName=nameList)
