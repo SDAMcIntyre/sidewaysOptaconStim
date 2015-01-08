b374df53b60bb676a1ca083a8fa45144ee2ae54e
SDAMcIntyre <sdamcintyre@gmail.com>
2015-01-08 18:14:30 +1100

    minor changes parameter values, trying to get quest to work, not quite there yet.

--- fingerTransferTMAEexperiment.py
+++ fingerTransferTMAEexperiment.py
@@ -1,29 +1,41 @@
 from noiseStaircaseHelpers import printStaircase, toStaircase
+from fingerTransferTMAEfunctions import stim_set
 import serial
 from psychopy import data, visual, event, core
 import csv
 import numpy
 from math import ceil
 import random
-print('after import')
 
-exptFolder = r'./questDemo/'
+setup = False
+exptFolder = r'./fingerTransferTest/'
+exptName = 'unadapted'
 
-stim_set(presentationTime = 3000, stepDuration = [50,50], pauseTime = 1000, responseTime = 1000, nReps =1, 
-                        standard = 82, comparison = [-104,-82,-60,-39,-17,17,39,60,82,104], exptFolder = r'./questDemo')
+standardValue = 82
+comparisonValues = [17,21,26,30,34,39,43,47,52,56,60,65,69,73,78,82,87,91,95,100,104,108,113,117,
+                                121,126,130,134,139,143,147,152,156,160,165,169,174,178,182,187,191,195,200,204,
+                                -17,-21,-26,-30,-34,-39,-43,-47,-52,-56,-60,-65,-69,-73,-78,-82,-87,-91,-95,-100,-104,
+                                -108,-113,-117,-121,-126,-130,-134,-139,-143,-147,-152,-156,-160,-165,-169,-174,-178,
+                                -182,-187,-191,-195,-200,-204]
 
-stimFile = '/Users/sarahmcintyre/ownCloud/Optacon Expts Sarah/Expt4_OptaconFingerTransferTMAE/sidewaysOptaconStim/questDemo/questDemo_stimList.dlm'
+if setup == True:
+    stim_set(presentationTime = 3000, stepDuration = [50,50], 
+                        standard = -standardValue, comparison = comparisonValues, #negative standard so positive means proximal motion and negative means distal (not the reverse)
+                        exptFolder = exptFolder, exptName = exptName, nReps =1) #pauseTime = 1000, 
+    core.quit()
+
+stimFile = exptFolder+exptName+'_stimList.dlm'
 with open(stimFile) as file_object:
     stimList = list(csv.DictReader(file_object, dialect='excel-tab'))
 
 isoiValues = []
 blockNs = []
 for stim in stimList:
-    isoiValues += [float(stim['isoi'])]
+    isoiValues += [-float(stim['isoi'])] #take the negative so positive means proximal motion and negative means distal (not the reverse)
     blockNs += [float(stim['blockNo_raw'])]
 isoiValues = numpy.array(isoiValues)
 blockNs = numpy.array(blockNs)
-prefaceValues = [-104,-82,-60,-39,-17,17,39,60,82,104]
+prefaceValues = [60,69,78,87,95,104]
 
 optacon=serial.Serial("/dev/tty.KeySerial1",9600,timeout=1)
 optacon.read(100) #check if there's a better way to clear optacon.read
@@ -35,21 +47,21 @@ comparisonLocation = 'right'
 threshCriterion = 0.5
 
 staircase = data.QuestHandler(startVal = 82, 
-                      startValSd = 150,
+                      startValSd = 12,
                       stopInterval= None, #6, #sd of posterior has to be this small or smaller for staircase to stop, unless nTrials reached
-                      nTrials=20,
+                      nTrials=32,
                       #extraInfo = thisInfo,
                       pThreshold = threshCriterion,
                       gamma = 0, #y value at floor of the function, 0 for PSE type experiment
                       delta=0.01, #lapse rate, I suppose for Weibull function fit
                       method = 'quantile', #uses the median of the posterior as the final answer
                       stepType = 'lin',  #will home in on the 80% threshold. But stepType = 'log' doesn't usually work
-                      minVal=min(isoiValues), maxVal = max(isoiValues)
+                      minVal=min(comparisonValues), maxVal = max(comparisonValues)
                       )
 
 expStop = False
 doingStaircasePhase = False #First phase of experiment is method of constant stimuli. If use naked QUEST, might converge too soon
-prefaceStaircaseTrialsN = 5
+prefaceStaircaseTrialsN = 12
 prefaceStaircaseTrials = random.sample(prefaceValues, len(prefaceValues))
 respEachTrial = list() #only needed for initialNonstaircaseTrials
 overallTrialN = -1
@@ -79,14 +91,15 @@ while (not staircase.finished) and expStop==False:
             staircase.importData( prefaceStaircaseTrials, numpy.array(respEachTrial)) 
         try: #advance the staircase
             suggested = staircase.next() #staircase suggests the next value
+            print '\nSuggested value = '+str(suggested)
             comparisonISOI = isoiValues[min(abs(suggested - isoiValues)) == abs(suggested - isoiValues)][0] #closest value to the suggested one that we have
             blockNo = int(blockNs[min(abs(suggested - isoiValues)) == abs(suggested - isoiValues)][0])
             overallTrialN += 1
         except StopIteration: #Need this here, even though test for finished above. I can't understand why finished test doesn't accomplish this.
-            print('stopping because staircase.next() returned a StopIteration, which it does when it is finished')
+            print('\n\nStopping because staircase.next() returned a StopIteration, which it does when it is finished')
             staircase.finished = True
             break #break out of the trials loop
-    print('overallTrialN=',overallTrialN, '   comparison ISOI for this trial = ', round(comparisonISOI,2)) #debugON
+    print '\noverallTrialN='+str(overallTrialN)+ '   comparison ISOI for this trial = '+ str(round(comparisonISOI,2))+"    Block number: "+str(blockNo)
     
     # next, present the stimulus and get a response
     keyPressed = ['']
@@ -95,7 +108,6 @@ while (not staircase.finished) and expStop==False:
         keyPressed = event.getKeys(keyList=quitkeys)
     
     optacon.write("b"+str(blockNo))
-    print("Block number: ",blockNo)
     msg = visual.TextStim(win, text='Sending next stimulus to Optacon\n<esc> to quit')
     msg.draw()
     win.flip()
@@ -119,40 +131,41 @@ while (not staircase.finished) and expStop==False:
         keyPressed = event.waitKeys(keyList=pedal+quitkeys)
         
     if keyPressed[0] in quitkeys:
-        print('user aborted')
+        print 'user aborted'
         expStop = True
     else:
         thisPedal = response[pedal.index(keyPressed[0])]
-        print thisPedal
         if thisPedal == comparisonLocation:
             thisResp = 1
         else:
             thisResp = 0
+        print 'pedal pressed: '+str(thisPedal)+', comparison location: '+str(comparisonLocation)
     
         # finally, record the response and the ISOI used
         respEachTrial.append(thisResp)
         if overallTrialN >= len(prefaceStaircaseTrials): #doing staircase trials now
             staircase.addResponse(thisResp, intensity = comparisonISOI)
-            print('Have added an intensity of ',comparisonISOI)
+            print 'Have added an intensity of ' + str(comparisonISOI)
 
 if overallTrialN+1 < len(prefaceStaircaseTrials) and (overallTrialN>=0): #exp stopped before got through staircase preface trials
     #add these non-staircase trials so QUEST knows about them
-    print('Importing ',respEachTrial,' and intensities ',prefaceStaircaseTrials)
-    staircase.importData( toStaircase(prefaceStaircaseTrials[0:overallTrialN+1],False), numpy.array(respEachTrial)) 
-print('Finished experiment.')
+    print 'Importing '+str(respEachTrial)+' and intensities '+str(prefaceStaircaseTrials[0:len(respEachTrial)])+'\n\n'
+    staircase.importData( toStaircase(prefaceStaircaseTrials[0:len(respEachTrial)],False), numpy.array(respEachTrial)) 
+print 'Finished experiment.'
 
 if staircase.finished:
-    print('Staircase was finished')
-    msg = visual.TextStim(win, text='Stop the Optacon protocol...\n<esc> to quit')
-    msg.draw()
-    win.flip()
-    while keyPressed[0] not in quitkeys:
-        keyPressed = event.waitKeys()
+    print 'Staircase completed.'
 else:
-    print('Staircase was not finished')
+    print 'Staircase did not complete.'
+
+msg = visual.TextStim(win, text='Stop the Optacon protocol.\nThen press <esc> to finish.')
+msg.draw()
+win.flip()
+while keyPressed[0] not in quitkeys:
+    keyPressed = event.waitKeys()
+win.close()
 
 print('Median of posterior distribution according to QUEST, ISOI= {:.4f}'.format(staircase.quantile())) 
 
-win.close()
-staircase.saveAsText(exptFolder+'/questDemo_data')
-printStaircase(staircase, False, briefTrialUpdate=True, printInternalVal=True,  alsoLog=False)
+staircase.saveAsText(exptFolder+exptName+'_data')
+printStaircase(staircase, False, briefTrialUpdate=False, printInternalVal=False,  alsoLog=False)
