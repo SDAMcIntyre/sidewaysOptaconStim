41c009faf0b916ab03fe351da8bb2e4e26a662ca
SDAMcIntyre <sdamcintyre@gmail.com>
2015-01-29 18:40:50 +1100

    implemented light trigger for presenting tactile stimulus

--- lightTrigger.py
+++ lightTrigger.py
@@ -1,7 +1,7 @@
 from psychopy import visual, core, event
 
-win = visual.Window(size=(1152, 870), fullscr=True, screen=0, allowGUI=False, allowStencil=False,
-    monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+win = visual.Window(size=(1152, 870), fullscr=True, screen=1, allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[-1,-1,-1], colorSpace='rgb',
     blendMode='avg', useFBO=True,
     )
 triggerPosition = [-11, 8]
@@ -12,12 +12,29 @@ triggerSensorOn = visual.Rect(win=win, name='polygon',units='cm',
     lineWidth=1, lineColor=[1,1,1], lineColorSpace='rgb',
     fillColor=[1,1,1], fillColorSpace='rgb',
     opacity=1,interpolate=True)
-    
-triggerSensorOn.setAutoDraw(True)
 
-triggerClockOn = core.Clock()
-while triggerClockOn.getTime()<5:
-    if event.getKeys(keyList=['escape','q']):
-        myWin.close()
-        core.quit()
+lightkeys = ['l','L']
+quitkeys = ['q','Q']
+userQuit = False
+while not userQuit:
+    keyPressed = ['']
+    msg = visual.TextStim(win, text='Press L key to trigger the light sensor. Press Q to escape')
+    msg.draw()
     win.flip()
+    while not any(keyPressed):
+        keyPressed = event.getKeys(keyList=lightkeys+quitkeys)
+    
+    if keyPressed[0] in quitkeys:
+        userQuit = True
+    
+    if keyPressed[0] in lightkeys:
+        print 'trigger'
+        triggerSensorOn.setAutoDraw(True)
+        triggerClockOn = core.Clock()
+        while triggerClockOn.getTime()<0.01:
+            win.flip()
+    triggerSensorOn.setAutoDraw(False)
+    win.flip()
+
+win.close()
+core.quit()
--- optaconSerialDemo.py
+++ optaconSerialDemo.py
@@ -1,16 +1,13 @@
 import serial
 from psychopy import data, visual, event, core
-import os
 
-exptFolder = r'./questDemo' 
-if not os.path.exists(exptFolder): 
-    os.makedirs(exptFolder)
-
+# define serial port to optacon computer
 optacon=serial.Serial("/dev/tty.KeySerial1",9600,timeout=1)
 optacon.read(100) #check if there's a better way to clear optacon.read
 
+# set up stimuli
 blocksToUse = range(2,5) # start at 2 because the first block should be blank (it is always played at the beginning)
-nRepeats = 5
+nRepeats = 2
 stimList = [{'blockNo':blockNo} for blockNo in blocksToUse] 
 trials = data.TrialHandler(stimList, nRepeats)
 trials.data.addDataType('repsonse')
@@ -19,51 +16,90 @@ pedal = ['a','c']
 response = ['left','right']
 quitkeys = ['escape','esc']
 
-win = visual.Window([1366, 768],fullscr=True) #[1680, 1050]
-msg = visual.TextStim(win, text='Start the Optacon protocol...\n<esc> to quit')
+# define window that everything appears in
+win = visual.Window(size=(1152, 870), fullscr=True, screen=1, allowGUI=False, allowStencil=False,
+    monitor='testMonitor', color=[-1,-1,-1], colorSpace='rgb',
+    blendMode='avg', useFBO=True,
+    )
+
+# define the patch of white screen to trigger the light sensor
+triggerPosition = [-11, 8]
+triggerSensorOn = visual.Rect(win=win, name='polygon',units='cm', 
+    width=[8, 8][0], height=[8, 8][1],
+    ori=0, pos=triggerPosition,
+    lineWidth=1, lineColor=[1,1,1], lineColorSpace='rgb',
+    fillColor=[1,1,1], fillColorSpace='rgb',
+    opacity=1,interpolate=True)
+    
+msg = visual.TextStim(win, text='Click \"Go" on the Optacon protocol...\n<esc> to quit')
 msg.draw()
 win.flip()
-
-nDone = 0
 optaconStatus = ''
+# wait until optacon protocol has been started
+while not optaconStatus == 'READY':
+    optaconStatus = optacon.read(size=5)
+    print optaconStatus
+    keyPressed = event.getKeys(keyList=quitkeys)
+    if any(keyPressed):
+        print 'user aborted'
+        core.quit()
+            
 for thisTrial in trials:
     keyPressed = ['']
-    while not any(keyPressed) and not optaconStatus == 'READY':
-        optaconStatus = optacon.read(size=5)
-        keyPressed = event.getKeys(keyList=quitkeys)
     
+    # tell optacon what the next stimulus is
+    msg = visual.TextStim(win, text='Loading next stimulus...\n<esc> to quit')
+    msg.draw()
+    win.flip()
     optacon.write("b"+str(thisTrial['blockNo']))
     print thisTrial['blockNo']
-    msg = visual.TextStim(win, text='Sending next stimulus to Optacon\n<esc> to quit')
-    msg.draw()
-    win.flip()
+    
+    # wait until optacon confirms next stimulus was received
     while not any(keyPressed) and not optaconStatus == "OK":
         optaconStatus = optacon.read(size=2)
+        print optaconStatus
         keyPressed = event.getKeys(keyList=quitkeys)
-    
-    # ###
-    # trigger optacon with light #
-    # ###
-    core.wait(0.5)
-    msg = visual.TextStim(win, text='Presenting stimulus...\n<esc> to quit')
+    if any(keyPressed):
+        print 'user aborted'
+        core.quit()
+        
+    # use light to trigger stimulus presentation
+    core.wait(1.2)
+    msg = visual.TextStim(win, text='\n<esc> to quit')
     msg.draw()
+    triggerSensorOn.setAutoDraw(True)
+    triggerClockOn = core.Clock()
+    while triggerClockOn.getTime()<0.01:
+        win.flip()
+    print triggerClockOn.getTime()
+    triggerSensorOn.setAutoDraw(False)
     win.flip()
     
-    core.wait(2)
+    # wait until optacon has finished presenting the stimulus
+    while not optaconStatus == 'READY':
+        optaconStatus = optacon.read(size=5)
+        print optaconStatus
+        keyPressed = event.getKeys(keyList=quitkeys)
+        if any(keyPressed):
+            print 'user aborted'
+            core.quit()
+    
+    # prompt user to make a response
     msg = visual.TextStim(win, text='Which stimulus ...? (L/R)\n<esc> to quit')
     msg.draw()
     win.flip()
     while not any(keyPressed):
         keyPressed = event.waitKeys(keyList=pedal+quitkeys)
-        
     if keyPressed[0] in quitkeys:
         print 'user aborted'
         core.quit()
+    msg = visual.TextStim(win, text='\n<esc> to quit')
+    msg.draw()
+    win.flip()
     
     thisResp = response[pedal.index(keyPressed[0])]
     print thisResp
     trials.data.add('response', thisResp)
-    nDone +=1
     
     
 print '\n'
@@ -73,10 +109,9 @@ trials.saveAsText(fileName='serialDemoData', # also write summary data to a text
                   stimOut=['blockNo'], 
                   dataOut=['response_raw'])
 
-msg = visual.TextStim(win, text='Stop the Optacon protocol...\n<esc> to quit')
+msg = visual.TextStim(win, text='Click \"Stop" on the Optacon protocol...\n<esc> to quit')
 msg.draw()
 win.flip()
 while keyPressed[0] not in quitkeys:
     keyPressed = event.waitKeys()
-    
 win.close()
