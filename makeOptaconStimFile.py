c1beff3bb7d8c21973c6c8e4029c2a712d082829
SDAMcIntyre <sdamcintyre@gmail.com>
2015-02-04 13:46:49 +1100

    explicitly set parameters for demo

--- makeOptaconStimFile.py
+++ makeOptaconStimFile.py
@@ -2,19 +2,20 @@
 """
 Created on Tue Nov  4 16:29:29 2014
 
-# New in this version:
-    ## make lead time in write_optacon_protocol_file() a separate block
-    ## put stim file in a specified folder
-    
 @author: sarahmcintyre
 """
 
 from optacon import *
 from optaconSideways import *
 
-motionStim, repList = single_presentation()
+motionStim, repList = single_presentation(presDur=3000, stepDur=[50,50], isoi=[82,82], 
+                                  rowsToUse=range(0,6), colsToUse=[range(0,6),range(18,24)],
+                                  stepVector=[1,1], randomPos=[False,False], spread=[True,True])
 
 exptPath = r'./example' 
 if not os.path.exists(exptPath): os.makedirs(exptPath)
 
-write_protocol_file(fileName='%s/sidewaysExample' %(exptPath),stimList=motionStim,stimRep=repList)
+write_protocol_file(fileName='%s/sidewaysExample' %(exptPath), stimList=motionStim,
+                                stimRep=repList, blockList=[2], dwellTime=[4.35], leadTime=2000,
+                                stimName=[' '], readPins=['A1','B1','C1','D1','E1','F1'])
+
