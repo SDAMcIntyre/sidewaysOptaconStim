e2037bba88306a83a82d13a688a5fff4f646c8d2
SDAMcIntyre <sdamcintyre@gmail.com>
2015-02-23 17:57:50 +1100

    added binomial glm fit to graphs

--- fingerTransferTest/analysis.R
+++ fingerTransferTest/analysis.R
@@ -22,6 +22,6 @@ ggplot(subset(data, nTrials > 4), aes(x=ISOI, y=pComp)) +geom_point(shape=4, siz
 #ggsave('pilot_sarah_unadapted.eps')
 #ggsave('pilot_sarah_unadapted.pdf')
 
-ggplot(subset(data, nTrials > 0), aes(x=ISOI, y=pComp)) +geom_text(aes(label=nTrials), size=3) +facet_wrap(~filename, scales = 'free_x')  +scale_y_continuous(limits=c(0,1)) +labs(title='Stadard ISOI = 82ms, prox/dist; duration = 50ms; numbers give N', y = 'Proportion comparison better direction signal', x = 'Comparison ISOI (ms), prox/dist, positive means matched to standard')
+ggplot(subset(data, nTrials > 0), aes(x=ISOI, y=pComp)) +geom_text(aes(label=nTrials), size=3) +facet_wrap(~filename, scales = 'free_x')  +scale_y_continuous(limits=c(0,1)) +stat_smooth(method = "glm", family = "binomial", se=F) +labs(title='Stadard ISOI = 82ms, prox/dist; duration = 50ms; numbers give N', y = 'Proportion comparison better direction signal', x = 'Comparison ISOI (ms), prox/dist, positive means matched to standard')
 #ggsave('pilot_sarah_unadapted.eps')
 #ggsave('pilot_sarah_unadapted.pdf')
